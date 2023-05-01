533e2d0a89b17145608755537ee90366f846525d
lllariogonzalez <lllariogonzalez@gmail.com>
2023-05-01 16:17:08 -0300

    Change remove categories

--- api/src/utils/chargeProducts.js
+++ api/src/utils/chargeProducts.js
@@ -2,12 +2,12 @@ const data = require('./../../productsInfo.json');
 const { Product, Category } = require('../db.js');
 
 
-const chargeProducts = async() =>{
+const chargeProducts = async () => {
     try {
-        const categories = [{ category: "Laptops"}, { category: "Smartphones"},{ category:"Tablets"},{ category:"Smartwatches"}, { category: "Speakers"}, { category:"TV"}];
-        const CategoryDB = categories.map(c=> Category.create(c));
-        await Promise.all(CategoryDB).then(()=>console.log('Categories successfully charged'));
-        const ProductDB= data.map((e) => Product.create({
+        const categories = [{ category: "Laptops" }, { category: "Smartphones" }, { category: "Tablets" }/* ,{ category:"Smartwatches"}, { category: "Speakers"}, { category:"TV"} */];
+        const CategoryDB = categories.map(c => Category.create(c));
+        await Promise.all(CategoryDB).then(() => console.log('Categories successfully charged'));
+        const ProductDB = data.map((e) => Product.create({
             name: e.name,
             image: e.image,
             description: e.description,
@@ -17,11 +17,11 @@ const chargeProducts = async() =>{
             stock: e.stock,
             brand: e.brand,
         }));
-        await Promise.all(ProductDB).then(()=>console.log('Product successfully charged'));
-        
+        await Promise.all(ProductDB).then(() => console.log('Product successfully charged'));
+
     } catch (error) {
         console.log(error.message);
     }
 }
 
-module.exports= chargeProducts;
+module.exports = chargeProducts;
